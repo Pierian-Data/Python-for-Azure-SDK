5292c0bf4507291b33919be19c7117e8a87a1bcf
bbig3831 <ben.bigelow@tutanota.com>
2023-04-05 13:15:11 -0700

    Add settings file for reuse of common variables

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+# Get the path to the directory this file is in
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+# Load environment variables
+load_dotenv(os.path.join(BASEDIR, 'config.env'))
+AZURE_SUBSCRIPTION_ID = os.getenv('AZURE_SUBSCRIPTION_ID')
