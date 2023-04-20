588e2511473f23c33654226e7bc91311c6556056
bbig3831 <ben.bigelow@tutanota.com>
2023-04-20 16:22:07 -0600

    Add notebook for creating storage and new requirements

--- settings.py
+++ settings.py
@@ -6,3 +6,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 # Load environment variables
 load_dotenv(os.path.join(BASEDIR, 'config.env'))
 AZURE_SUBSCRIPTION_ID = os.getenv('AZURE_SUBSCRIPTION_ID')
+
+# Default variables
+DEFAULT_RESOURCE_GROUP = 'default-resource-group'
+DEFAULT_LOCATION = 'eastus'
