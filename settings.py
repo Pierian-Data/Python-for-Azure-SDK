aa74a34a5c5ce57a7beab40435853de3129e8343
bbig3831 <ben.bigelow@tutanota.com>
2023-04-21 16:35:08 -0600

    Update environment variables and requirements

--- settings.py
+++ settings.py
@@ -4,8 +4,12 @@ from dotenv import load_dotenv
 # Get the path to the directory this file is in
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 # Load environment variables
-load_dotenv(os.path.join(BASEDIR, 'config.env'))
+load_dotenv(os.path.join(BASEDIR, '.env'))
 AZURE_SUBSCRIPTION_ID = os.getenv('AZURE_SUBSCRIPTION_ID')
+CONNECTION_STRING = os.getenv('CONNECTION_STRING')
+AZURE_CLIENT_ID = os.getenv('AZURE_CLIENT_ID')
+AZURE_TENANT_ID = os.getenv('AZURE_TENANT_ID')
+AZURE_CLIENT_SECRET = os.getenv('AZURE_CLIENT_SECRET')
 
 # Default variables
 DEFAULT_RESOURCE_GROUP = 'default-resource-group'
